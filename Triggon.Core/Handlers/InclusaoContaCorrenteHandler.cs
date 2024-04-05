b9d61de17b13d248a2523d38f9c6a54c1fe3ec4a
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2024-04-05 17:40:20 -0300

    arquitetura tdd

---
+++ Triggon.Core/Handlers/InclusaoContaCorrenteHandler.cs
@@ -0,0 +1,73 @@
+﻿namespace Triggon.Core.Handlers;
+public class InclusaoContaCorrenteHandler
+{
+    private readonly IGenericRepository<Conta> _repository;
+    private readonly IGenericProducer<ContaCriadaEvent> _producer;
+
+    public async Task<ContaCommandResponse> Handle(ContaCommand request, CancellationToken cancellationToken = default)
+    {
+        Conta entity = request.ToEntity();
+        await _repository.Insert(entity, cancellationToken);
+
+        ContaCriadaEvent @event = entity.ToEvent();
+        await _producer.Send(@event, cancellationToken);
+
+        return new ContaCommandResponse();
+    }
+}
+
+public class Conta
+{
+    public ContaCriadaEvent ToEvent()
+    {
+        return new ContaCriadaEvent();
+    }
+}
+
+public record ContaCommand
+{
+    public Conta ToEntity()
+    {
+        return new Conta();
+    }
+}
+
+public record ContaCriadaEvent
+{
+}
+
+public record ContaCommandResponse
+{
+}
+
+public interface IGenericConsumer<T> where T : class
+{
+    Task<T> Consume(CancellationToken cancellationToken = default);
+}
+
+public interface IContaConsumer : IGenericConsumer<Conta>
+{
+}
+
+public interface IGenericProducer<T> where T : class
+{
+    Task Send(T entity, CancellationToken cancellationToken = default);
+}
+public interface IContaProducer : IGenericProducer<Conta>
+{
+}
+
+public interface IGenericRepository<T> where T : class
+{
+    Task Insert(T entity, CancellationToken cancellationToken = default);
+}
+public interface IContaRepository : IGenericRepository<Conta>
+{
+}
+
+public interface IGenericHttpClient<T> where T : class
+{
+}
+public interface IContaHttpClient : IGenericHttpClient<Conta>
+{
+}
