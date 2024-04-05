0a84cc6dc031779c91fef12d2c36491702c83b7b
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2024-04-05 19:29:33 -0300

    arquitetura repository Mongo by context

--- Triggon.Api/Controllers/EmprestimosController.cs
+++ Triggon.Api/Controllers/EmprestimosController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using Triggon.Core.Contexts;
 using Triggon.Core.Entities;
 using Triggon.Core.Features;
 using Triggon.Core.Repositories;
@@ -15,5 +17,27 @@ public class EmprestimosController : ControllerBase
     {
         await repository.InsertAsync(solicitacao.ToEntity(), cancellationToken);
         return Ok();
+    }
+
+    [HttpGet("/teste")]
+    public async Task<IActionResult> SomeAction([FromServices] IMyApplicationContext _context, CancellationToken cancellationToken)
+    {
+        return Ok(await _context.Users.Find(x => x.Id != Guid.Empty).ToListAsync(cancellationToken));
+    }
+
+    [HttpPost("/teste")]
+    public async Task<IActionResult> SomeActionPost([FromServices] IMyApplicationContext _context, CancellationToken cancellationToken)
+    {
+        await _context.Users.InsertOneAsync(new Usuario() { Id = Guid.NewGuid() }, cancellationToken);
+        return Created();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SomeAction([FromServices] IHttpClientFactory httpClientFactory)
+    {
+        // Get an HttpClient configured to the specification you defined in StartUp.
+        var client = httpClientFactory.CreateClient("Github");
+
+        return Ok(await client.GetStringAsync("/someapi"));
     }
 }
--- Triggon.Api/Program.cs
+++ Triggon.Api/Program.cs
@@ -1,7 +1,22 @@
+using Polly;
 using Triggon.Api;
+using Triggon.Core.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddRepository();
+builder.Services.AddRepository()
+                .AddMongoUoW();
+//Adicionar HTTPCLIENT
+builder.Services.AddHttpClient("Github", client =>
+{
+    client.BaseAddress = new Uri("https://api.github.com/");
+    client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+})
+.AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(new[]
+{
+    TimeSpan.FromSeconds(1),
+    TimeSpan.FromSeconds(5),
+    TimeSpan.FromSeconds(10)
+}));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
